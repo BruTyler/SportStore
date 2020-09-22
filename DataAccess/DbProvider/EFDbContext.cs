9bfcaa784b0126591b4ac358e6ab1f15c872f0fd
Anton Churkin <afrg@yandex.ru>
2020-09-22 16:10:26 +0500

    add unit test with EfContext in memory

--- DataAccess/DbProvider/EFDbContext.cs
+++ DataAccess/DbProvider/EFDbContext.cs
@@ -1,10 +1,21 @@
-﻿using System.Data.Entity;
+﻿using System.Data.Common;
+using System.Data.Entity;
 using SportsStore.Domain.Entities;
 
 namespace DataAccess.DbProvider
 {
     public class EFDbContext: DbContext
     {
+        public EFDbContext() : base()
+        {
+            //default constructor for production
+        }
+
+        public EFDbContext(DbConnection connection) : base(connection, false)
+        {
+            //additional constructor for injecting in unit-test
+        }
+
         public DbSet<Product> Products { get; set; }
     }
 }
---
+++ SportsStore.UnitTests/EFContextTest.cs
@@ -0,0 +1,55 @@
+﻿using System.Data.Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DataAccess.DbProvider;
+using SportsStore.Domain.Entities;
+
+namespace SportsStore.UnitTests
+{
+    [TestClass]
+    public class EFContextTest
+    {
+        private EFDbContext _context;
+
+        [TestInitialize]
+        public void SetupTest()
+        {
+            //init Ef database-in-memory
+            DbConnection connection = Effort.DbConnectionFactory.CreateTransient();
+            _context = new EFDbContext(connection);
+        }
+
+        [TestMethod]
+        public void AddProduct()
+        {
+            _context.Products.Add(new Product()
+            {
+                Category = "category",
+                Name = "test name",
+                Description = "descr",
+                Price = 1,
+            });
+            
+            var succeedStoredCounter  = _context.SaveChanges();
+
+            Assert.AreEqual(1, succeedStoredCounter);
+        }
+
+        [TestMethod]
+        public void AddAndReadProduct()
+        {
+            _context.Products.Add(new Product()
+            {
+                Category = "category",
+                Name = "test name",
+                Description = "descr",
+                Price = 1,
+            });
+
+            var succeedStoredCounter = _context.SaveChanges();
+            var storedProd = _context.Products.Find(1);
+
+            Assert.AreEqual(1, succeedStoredCounter);
+            Assert.AreEqual("descr", storedProd.Description);
+        }
+    }
+}
